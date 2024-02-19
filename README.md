5e023d6db3fda131cdaf2be312fdb60c9e7d1965
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-02-19 10:32:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Simple Call Limiter
+
+A simple function rate limiter that delays execution of functions based on a specified delay.
+
+## Installation
+
+You can install simple-call-limiter via npm:
+
+```bash
+npm install simple-call-limiter
+```
+
+## API
+
+### SimpleLimiter(delayMs: number)
+
+Creates a new instance of SimpleDelay with the specified delay in milliseconds.
+
+- `delayMs`: The delay in milliseconds between function calls.
+
+### run(fn, ...args)
+
+Runs the provided function after ensuring the specified delay. If multiple calls are made within the delay period, they are queued and executed sequentially.
+
+- `fn`: The function to be executed.
+- `args`: Arguments to be passed to the function.
+
+Returns a Promise that resolves when the function is executed. If you are using Typescript, the type of the Promise is the output type of function.
+
+## Example
+
+```javascript
+import SimpleLimiter from "simple-call-limiter";
+
+// Create a new instance of SimpleLimiter with a delay of 1000 milliseconds
+const limiter = new SimpleLimiter(1000);
+
+for (let i = 0; i < 10; i++) {
+	limiter.run(console.log, i);
+}
+
+// output
+// 0 (immediate)
+// 1 (after 1 sec)
+// 2 (after 2 sec)
+// 3 (after 3 sec)
+// 4 (after 4 sec)
+// 5 (after 5 sec)
+// 6 (after 6 sec)
+// 7 (after 7 sec)
+// 8 (after 8 sec)
+// 9 (after 9 sec)
+```
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+/**
+ * A simple function rate limiter that delays execution of functions based on a specified delay.
+ */
+export default class SimpleLimiter {
+	/**
+	 * Queue to hold functions and their arguments to be executed.
+	 */
+	queue: {
+		fn: Function;
+		args: any[];
+	}[];
+
+	/**
+	 * Timestamp of the last function call.
+	 */
+	lastCallTime: number;
+
+	/**
+	 * Delay in milliseconds between function calls.
+	 */
+	delayMs: number;
+
+	/**
+	 * Creates an instance of RateLimit.
+	 * @param {number} delayMs - The delay in milliseconds between function calls.
+	 */
+	constructor(delayMs: number) {
+		this.queue = [];
+		this.lastCallTime = 0;
+		this.delayMs = delayMs;
+	}
+
+	/**
+	 * Runs the provided function after ensuring the specified delay.
+	 * If multiple calls are made within the delay period, they are queued and executed sequentially.
+	 * @param {Function} fn - The function to be executed.
+	 * @param {...any[]} args - Arguments to be passed to the function.
+	 * @returns {Promise<T>} A Promise that resolves when the function is executed.
+	 */
+	run<T>(fn: (...args: any[]) => T, ...args: any[]): Promise<T> {
+		return new Promise<T>((resolve) => {
+			const currentTime = new Date().getTime();
+			const timeSinceLastCall = currentTime - this.lastCallTime;
+
+			const executeFunction = () => {
+				this.lastCallTime = new Date().getTime();
+				resolve(fn(...args));
+			};
+
+			if (timeSinceLastCall >= this.delayMs) {
+				executeFunction();
+			} else {
+				this.queue.push({ fn, args });
+
+				setTimeout(() => {
+					if (this.queue.length > 0) {
+						const call = this.queue.shift();
+
+						if (call) {
+							executeFunction();
+						}
+					}
+				}, this.delayMs * this.queue.length - timeSinceLastCall);
+			}
+		});
+	}
+}
